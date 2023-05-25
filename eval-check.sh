4eb29f657c3e275935e964233a9b60c8642e2d0a
Thaumy <thaumy@outlook.com>
2023-05-25 17:46:52 +0800

    + eval-check script

---
+++ eval-check.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+nix-env -f . -qa \* --meta \
+  --allowed-uris https://static.rust-lang.org \
+  --option restrict-eval true \
+  --option allow-import-from-derivation true \
+  --drv-path --show-trace \
+  -I nixpkgs=$(nix-instantiate --find-file nixpkgs) \
+  -I ./ \
+  --json | jq -r 'values | .[].name'
