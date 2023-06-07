211ada5b86145b450e7250f6775816a06cb2786b
Thaumy <thaumy@outlook.com>
2023-06-07 08:33:05 +0800

    fix eval check

--- eval-check.sh
+++ eval-check.sh
@@ -2,11 +2,10 @@
 
 export NIX_PATH=nixpkgs=channel:nixpkgs-unstable
 
-nix-env -f . -qa \* --meta \
+nix-env -f . -qa \* --meta --xml \
   --allowed-uris https://static.rust-lang.org \
   --option restrict-eval true \
   --option allow-import-from-derivation true \
   --drv-path --show-trace \
   -I nixpkgs=$(nix-instantiate --find-file nixpkgs) \
-  -I $PWD \
-  --json
+  -I $PWD
--- pkgs/idbuilder/default.nix
+++ pkgs/idbuilder/default.nix
@@ -58,14 +58,14 @@ let
     packageJSON = "${src_idbuilder}/package.json";
   };
 
-  # rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  # rust-overlay = import (fetchFromGitHub {
+  #   owner = "oxalica";
+  #   repo = "rust-overlay";
+  #   rev = "9ea38d547100edcf0da19aaebbdffa2810585495";
+  #   sha256 = "kwKCfmliHIxKuIjnM95TRcQxM/4AAEIZ+4A9nDJ6cJs=";
+  # });
 
-  rust-overlay = import (fetchFromGitHub {
-    owner = "oxalica";
-    repo = "rust-overlay";
-    rev = "9ea38d547100edcf0da19aaebbdffa2810585495";
-    sha256 = "kwKCfmliHIxKuIjnM95TRcQxM/4AAEIZ+4A9nDJ6cJs=";
-  });
+  rust-overlay = import ../rust-overlay;
 
   extended-pkgs = pkgs.extend (rust-overlay);
 
